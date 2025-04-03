2a2636429a008e7c5b03479ab75137fdd584317b
mohamedktw <m.abukhatwa@gmail.com>
2025-04-03 06:21:27 -0400

    added create Employee API

---
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,25 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+}
